3b1afbbd4f26555f253a51669d8cfd9eb48f1682
lukas-hornacek <lukas.hornacek1928@gmail.com>
2025-02-23 15:02:05 +0100

    add GitHub actions for deployment of main branch

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "https://lukas-hornacek.github.io/idktest/",
-  build: {
-    outDir: 'docs',
-    emptyOutDir: true
-  }
 })
