24bb5b0b41d0509c277c881d94fa12be384afe06
lukas-hornacek <lukas.hornacek1928@gmail.com>
2025-02-23 15:34:35 +0100

    add example test

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://lukas-hornacek.github.io/idktest/",
+  base: "https://lukas-hornacek.github.io/fojalka/",
 })
