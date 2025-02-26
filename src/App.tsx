9a0cc79f6a27df41ec76da041c8a060d19edc02d
lukas-hornacek <lukas.hornacek1928@gmail.com>
2025-02-26 18:14:25 +0100

    add example cytoscape initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,60 @@
+import cytoscape from 'cytoscape';
+
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div>Víla Fojálka</div>
-  );
+// Example initialization of the Cytoscape library in a React component
+export default class App extends Component {
+    cy: cytoscape.Core | undefined;
+
+    componentDidMount() {
+        this.cy = cytoscape({
+            container: document.getElementById("cy"),
+
+            elements: [
+                { group: 'nodes', data: { id: 'x' }, position: { x: 100, y: 100 } },
+                { group: 'nodes', data: { id: 'y' }, position: { x: 300, y: 100 } },
+                { group: 'edges', data: { id: 'a', source: 'x', target: 'y' } },
+            ],
+
+            style: [
+                {
+                    selector: 'node',
+                    style: {
+                        'background-color': '#666',
+                        'label': 'data(id)'
+                    }
+                },
+        
+                {
+                    selector: 'edge',
+                    style: {
+                        'width': 3,
+                        'line-color': '#ccc',
+                        'target-arrow-color': '#ccc',
+                        'target-arrow-shape': 'triangle',
+                        'curve-style': 'bezier',
+                        'label': 'data(id)'
+                    }
+                }
+            ],
+        
+            layout: {
+                name: 'preset',
+            }
+        });
+    }
+
+    componentWillUnmount() {
+        this.cy?.destroy();
+    }
+
+    render() {
+        return (
+            <>
+                <h1>Víla Fojálka</h1>
+                <div id='cy'></div>
+            </>
+        );
+    }
 }
-
-export default App;
