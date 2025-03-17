f219fb22a4e114fe11034366afda9d5701488705
radoslavPetrani <radoslavpetrani@gmail.com>
2025-03-17 18:50:46 +0100

    test Commit

--- src/types/visitors/configuration.ts
+++ src/types/visitors/configuration.ts
@@ -14,7 +14,19 @@ export class NextStepVisitor implements IConfigurationVisitor {
 
     visitFiniteConfiguration(configuration: FiniteConfiguration): FiniteConfiguration {
         // TODO implement: Based on this.automaton and configuration, calculate the next step configuration for FA
-        return configuration; // TODO remove, just a dummy return
+        const nextSymbol = configuration.remainingInput[0];
+
+        let nextState: string | undefined;
+        this.automaton.deltaFunctionMatrix[configuration.stateId].array.forEach(edge => {
+            if (edge.inputChar == nextSymbol) nextState = edge.id
+        });
+
+        if (nextState === undefined)    return configuration;
+                else {
+                    const t = this.automaton.deltaFunctionMatrix[configuration.stateId][nextState];
+                    return configuration
+                }
+
     };
 
     visitPDAConfiguration(configuration: PDAConfiguration): PDAConfiguration {
