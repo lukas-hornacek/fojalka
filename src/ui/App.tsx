fc2397018c3549e51665fb11c8e6e6adaa6ca463
lukas-hornacek <lukas.hornacek1928@gmail.com>
2025-03-20 15:01:45 +0100

    add button to change between one and two windows

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,17 +1,14 @@
 import "./App.css";
-import AutomatonWindow from "./AutomatonWindow";
 import AddStateButton from "./AddStateButton";
 import { CoreProvider } from "../core/CoreContext";
+import VisualWindows from "./VisualWindows";
 
 export default function App() {
   return (
     <CoreProvider>
       <h1>Víla Fojálka</h1>
       <AddStateButton />
-      <div id="cy-window" className="d-flex flex-row">
-        <AutomatonWindow id="cy-primary" />
-        <AutomatonWindow id="cy-secondary" />
-      </div>
+      <VisualWindows />
     </CoreProvider>
   );
 }
