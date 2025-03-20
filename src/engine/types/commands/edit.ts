115f0eb4281846c47ebf5b74352b1380949a68dd
lukas-hornacek <lukas.hornacek1928@gmail.com>
2025-03-20 18:44:01 +0100

    implement remove commands

--- src/engine/types/commands/edit.ts
+++ src/engine/types/commands/edit.ts
@@ -24,11 +24,11 @@ export class AddEdgeCommand extends EditCommand {
       );
     }
 
-    if (this.automaton.deltaFunctionMatrix?.[this.fromStateId] === undefined) {
+    if (this.automaton.deltaFunctionMatrix[this.fromStateId] === undefined) {
       this.automaton.deltaFunctionMatrix[this.fromStateId] = {};
     }
 
-    if (this.automaton.deltaFunctionMatrix?.[this.fromStateId]?.[this.toStateId] === undefined) {
+    if (this.automaton.deltaFunctionMatrix[this.fromStateId][this.toStateId] === undefined) {
       this.automaton.deltaFunctionMatrix[this.fromStateId][this.toStateId] = [];
     }
 
@@ -59,6 +59,75 @@ export class AddStateCommand extends EditCommand {
 
     this.saveBackup();
     this.automaton.states.push(this.stateId);
+  }
+}
+
+export class RemoveStateCommand extends EditCommand {
+  stateId: string;
+
+  constructor(_automaton: IAutomaton, _stateId: string) {
+    super(_automaton);
+    this.stateId = _stateId;
+  }
+
+  execute(): IErrorMessage | undefined {
+    const index = this.automaton.states.findIndex(id => id === this.stateId);
+    if (index === -1) {
+      return new ErrorMessage(`Cannot remove state ${this.stateId}, as it does not exist.`);
+    }
+
+    this.saveBackup();
+
+    // remove state
+    if (this.automaton.states.length > 1) {
+      this.automaton.states[index] = this.automaton.states.pop()!;
+    } else {
+      this.automaton.states.pop();
+    }
+
+    // remove edges going from stateId
+    this.automaton.deltaFunctionMatrix[this.stateId] = {};
+
+    // remove edges going to stateId
+    for (const from in this.automaton.deltaFunctionMatrix) {
+      for (const to in this.automaton.deltaFunctionMatrix[from]) {
+        if (to === this.stateId) {
+          this.automaton.deltaFunctionMatrix[from][to] = [];
+        }
+      }
+    }
+  }
+}
+
+export class RemoveEdgeCommand extends EditCommand {
+  edgeId: string;
+  fromStateId: string;
+  toStateId: string;
+
+  constructor(_automaton: IAutomaton, _fromStateId: string, _toStateId: string, _edgeId: string) {
+    super(_automaton);
+    this.edgeId = _edgeId;
+    this.fromStateId = _fromStateId;
+    this.toStateId = _toStateId;
+  }
+
+  execute(): IErrorMessage | undefined {
+    const index = this.automaton.deltaFunctionMatrix[this.fromStateId]?.[this.toStateId]?.findIndex(edge => edge.id === this.edgeId);
+    if (index === undefined) {
+      return new ErrorMessage(`Cannot remove edge ${this.edgeId} because one of its states is not incorrect.`);
+    }
+    if (index === -1) {
+      return new ErrorMessage(`Cannot remove edge ${this.edgeId}, as it does not exist.`);
+    }
+
+    this.saveBackup();
+
+    if (this.automaton.deltaFunctionMatrix[this.fromStateId][this.toStateId].length > 1) {
+      this.automaton.deltaFunctionMatrix[this.fromStateId][this.toStateId][index] =
+        this.automaton.deltaFunctionMatrix[this.fromStateId][this.toStateId].pop()!;
+    } else {
+      this.automaton.deltaFunctionMatrix[this.fromStateId][this.toStateId].pop();
+    }
   }
 }
 
