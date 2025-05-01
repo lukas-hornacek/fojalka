c7384eaa93ed0c601608594fe529124fb33e64f6
LukasHudcovsky <luki.hudcovsky@gmail.com>
2025-05-01 14:32:37 +0200

    add test for grammar to automaton

--- tests/grammarToAutomaton.test.ts
+++ tests/grammarToAutomaton.test.ts
@@ -1,10 +1,12 @@
 import { expect, test } from "vitest";
-import { GrammarNormalFormAlgorithm } from "../src/engine/algorithm";
+import { GrammarNormalFormAlgorithm, GrammarToAutomatonAlgorithm } from "../src/engine/algorithm";
 import { ModeHolder } from "../src/core/core";
 import { EPSILON, INITIAL_NONTERMINAL } from "../src/constants";
 import { GrammarEditCommand } from "../src/engine/grammar/commands/edit";
 import { Grammar, GrammarType } from "../src/engine/grammar/grammar";
 import { GrammarCore } from "../src/core/grammarCore";
+import { AutomatonEditCommand } from "../src/engine/automaton/commands/edit";
+import { AutomatonType } from "../src/engine/automaton/automaton";
 
 test("testing grammar normal form", () => {
   const core = new GrammarCore(GrammarType.REGULAR, new ModeHolder());
@@ -79,3 +81,79 @@ test("testing grammar normal form", () => {
   expect(outputWords).toContainEqual(["B"]);
   expect(outputWords).toContainEqual(["a", "ψ2,1"]);
 });
+
+test("testing grammar to automaton", () => {
+  const core = new GrammarCore(GrammarType.REGULAR, new ModeHolder());
+  let algorithm = new GrammarToAutomatonAlgorithm(core);
+
+  core.grammar = new Grammar(GrammarType.REGULAR, [INITIAL_NONTERMINAL, "A", "B"], ["a", "b"], INITIAL_NONTERMINAL);
+  let rule = core.factory.createProductionRule(INITIAL_NONTERMINAL, ["a", "b", "a", INITIAL_NONTERMINAL], core.grammar);
+  core.grammar.productionRules.push(rule);
+  rule = core.factory.createProductionRule(INITIAL_NONTERMINAL, ["b", "A"], core.grammar);
+  core.grammar.productionRules.push(rule);
+  rule = core.factory.createProductionRule(INITIAL_NONTERMINAL, ["B"], core.grammar);
+  core.grammar.productionRules.push(rule);
+  rule = core.factory.createProductionRule(INITIAL_NONTERMINAL, ["b"], core.grammar);
+  core.grammar.productionRules.push(rule);
+  rule = core.factory.createProductionRule("A", ["a", "A"], core.grammar);
+  core.grammar.productionRules.push(rule);
+  rule = core.factory.createProductionRule("A", [EPSILON], core.grammar);
+  core.grammar.productionRules.push(rule);
+  rule = core.factory.createProductionRule("B", ["B"], core.grammar);
+  core.grammar.productionRules.push(rule);
+  rule = core.factory.createProductionRule("B", ["a", "a"], core.grammar);
+  core.grammar.productionRules.push(rule);
+
+  expect(() => algorithm.init(new ModeHolder())).toThrowError("Cannot use algorithm, as the grammar is not in required normal form.");
+
+  const algorithm2 = new GrammarNormalFormAlgorithm(core);
+  const core2 = algorithm2.init(new ModeHolder());
+
+  //trnsform grammar into normal form
+  let result = algorithm2.next();
+  while (result !== undefined) {
+    core2.grammar.executeCommand((result.command as GrammarEditCommand));
+    result = algorithm2.next();
+  }
+
+  algorithm = new GrammarToAutomatonAlgorithm(core2);
+  const core3 = algorithm.init(new ModeHolder());
+
+  //running the algorithm
+  result = algorithm.next();
+  while (result !== undefined) {
+    core3.automaton.executeCommand((result.command as AutomatonEditCommand));
+    result = algorithm.next();
+  }
+
+  //testing properitie of the new automaton
+  expect(core3.automaton.states).toEqual([INITIAL_NONTERMINAL, "A", "B", "ψ1,1", "ψ1,2", "ψ2,1", "fin"]);
+  expect(core3.automaton.initialStateId).toBe(INITIAL_NONTERMINAL);
+  expect(core3.automaton.finalStateIds).toEqual(["fin"]);
+  expect(core3.automaton.automatonType).toBe(AutomatonType.FINITE);
+
+  for (const from in core3.automaton.deltaFunctionMatrix) {
+    for (const to in core3.automaton.deltaFunctionMatrix[from]) {
+      expect(core3.automaton.deltaFunctionMatrix[from][to]).toHaveLength(1);
+    }
+  }
+
+  expect(core3.automaton.deltaFunctionMatrix[INITIAL_NONTERMINAL]["A"][0].inputChar).toBe("b");
+  expect(core3.automaton.deltaFunctionMatrix[INITIAL_NONTERMINAL]["fin"][0].inputChar).toBe("b");
+  expect(core3.automaton.deltaFunctionMatrix[INITIAL_NONTERMINAL]["B"][0].inputChar).toBe(EPSILON);
+  expect(core3.automaton.deltaFunctionMatrix[INITIAL_NONTERMINAL]["ψ1,1"][0].inputChar).toBe("a");
+
+  expect(core3.automaton.deltaFunctionMatrix["A"]["A"][0].inputChar).toBe("a");
+  expect(core3.automaton.deltaFunctionMatrix["A"]["fin"][0].inputChar).toBe(EPSILON);
+
+  expect(core3.automaton.deltaFunctionMatrix["B"]["ψ2,1"][0].inputChar).toBe("a");
+  expect(core3.automaton.deltaFunctionMatrix["B"]["B"][0].inputChar).toBe(EPSILON);
+
+  expect(core3.automaton.deltaFunctionMatrix["ψ1,1"]["ψ1,2"][0].inputChar).toBe("b");
+
+  expect(core3.automaton.deltaFunctionMatrix["ψ1,2"][INITIAL_NONTERMINAL][0].inputChar).toBe("a");
+
+  expect(core3.automaton.deltaFunctionMatrix["ψ2,1"]["fin"][0].inputChar).toBe("a");
+
+});
+
