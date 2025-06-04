812ebae2d93a8ce5f4ba467798ff05f9acea2ccf
cxivo <marek14michalovic@gmail.com>
2025-06-04 11:43:52 +0200

    fix the stupid broken UI, still not sure why the hell this was the fix

--- src/ui/AutomatonWindow.tsx
+++ src/ui/AutomatonWindow.tsx
@@ -20,7 +20,9 @@ export default function AutomatonWindow({ primary }: { primary: boolean }) {
     } else {
       throw new Error("AutomatonWindow could not be initialized correctly");
     }
-  }, [primary, coreContext]); // removed coreContext from here, because it was causing trouble - init already gets called from import
+  }, [primary, coreContext]);
+  // removed coreContext from here, because it was causing trouble - init already gets called from import
+  // nvm readded it
 
   return <div id={primary ? PRIMARY_CYTOSCAPE_ID : SECONDARY_CYTOSCAPE_ID}></div>;
 }
--- src/ui/MainMenu.tsx
+++ src/ui/MainMenu.tsx
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.css";
 import { useContext, useState } from "react";
 import { ICoreType, Kind, Mode, ObjectType } from "../core/core";
 import {
